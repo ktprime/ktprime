8fa2167aea056bc80dc5fa6e4c1091fafdb1e622
hyb <bailuzhou@163.com>
2018-09-01 12:35:55 +0800

    fix merge2

--- topK.cpp
+++ topK.cpp
@@ -1,42 +1,3 @@
-/***
-maxn = 100000000, topk = 10000, type = 0
-stl nth_element  1144 ms
-radix_sort        200 ms
-stl priority_queue 108 ms
-my max_heap      152 ms
-stl make_heap    108 ms
-sort-merge2      112 ms
-sort-merge       108 ms
-
-maxn = 100000000, topk = 10000, type = 1
-stl nth_element   128 ms
-radix_sort        400 ms
-stl priority_queue 92 ms
-my max_heap      140 ms
-stl make_heap     92 ms
-sort-merge2       96 ms
-sort-merge        96 ms
-
-maxn = 100000000, topk = 10000, type = 3
-stl nth_element   368 ms
-radix_sort        216 ms
-stl priority_queue 116 ms
-my max_heap      160 ms
-stl make_heap    112 ms
-sort-merge2      116 ms
-sort-merge       112 ms
-
-maxn = 100000000, topk = 10000, type = 2
-stl nth_element   128 ms
-radix_sort        400 ms
-stl priority_queue 96 ms
-my max_heap      140 ms
-stl make_heap     96 ms
-sort-merge2       96 ms
-sort-merge       136 ms
-*/
-
-
 #include <algorithm>
 #include <numeric>
 #include <queue>
@@ -45,9 +6,10 @@ sort-merge       136 ms
 #include <cstdlib>
 #include <ctime>
 #include <cassert>
+#include <climits>
 
 #define  MAXN  10000*10000
-#define  MAXK  10000
+#define  MAXK  100000
 
 using namespace std;
 
@@ -55,15 +17,21 @@ template<class T>
 class maxheap
 {
 public:
-	maxheap() { size = 0; a[0] = 1 << 30; }
-	T top()   { return a[1]; }
+	maxheap(int maxk) {
+		size = 0;
+		a[0] = INT_MAX;
+		for (int i = 0; i <= maxk; i++)
+			a[maxk + i] = INT_MIN;
+	}
 
-	void push(const T& x)
+	T top() const { return a[1]; }
+
+	void push(const T x)
 	{
 		int c = ++size;
 		int p = size / 2;
 
-		while (x > a[p] && p >= 1) {
+		while (x > a[p] /*&& p >= 1*/) {
 			a[c] = a[p];
 			c = p;
 			p /= 2;
@@ -73,11 +41,10 @@ public:
 
 	void pop()
 	{
-		const T& x = a[size--];
-		int p = 1;
-		int c = 1;
+		const T x = a[size--];
+		int p = 1, c = 1;
 
-		while (c <= size && x < a[c]) {
+		while (x < a[c]/* && c <= size &&*/) {
 			a[p] = a[c];
 			p = c;
 			c *= 2;
@@ -95,17 +62,12 @@ public:
 
 static int buff[MAXN + 2];
 
-void reset(int a[], int n)
-{
-	memcpy(a, buff, n * sizeof(a[0]));
-}
-
 void rand_swap(int a[], int n, int k)
 {
 	const int step = n / k;
 //	std::sort(a, a + k);
 	for (int i = 1; i < k; i ++) {
-#if 1
+#if RD
 		int h = rand() % k, t = i * step + rand() % step;
 		if (a[h] > a[t])
 			std::swap(a[h], a[t]);
@@ -116,10 +78,17 @@ void rand_swap(int a[], int n, int k)
 	}
 }
 
+void reset(int a[], int n)
+{
+	memcpy(a, buff, n * sizeof(a[0]));
+	rand_swap(a, n, 10000);
+}
+
 #if __linux__
 #include <sys/resource.h>
 #elif _WIN32
 //#include <windows.h>
+#include <intrin.h>
 #endif
 
 static clock_t getTime()
@@ -136,7 +105,7 @@ static clock_t getTime()
 	long usec = rup.ru_utime.tv_usec + rup.ru_stime.tv_usec;
 	return sec * 1000 + usec / 1000;
 #else
-	return clock() / 1000;
+	return clock();
 #endif
 }
 
@@ -152,7 +121,6 @@ void stl_sort(int a[], int n, const int k)
 void stl_nth(int a[], int n, const int k)
 {
 	reset(a, n);
-	rand_swap(a, n, k);
 	auto ts = getTime();
 
 	std::nth_element(a, a + k, a + n);
@@ -164,7 +132,6 @@ void stl_nth(int a[], int n, const int k)
 void stl_makeheap(int a[], int n, const int k)
 {
 	reset(a, n);
-	rand_swap(a, n, k);
 	auto ts = getTime();
 
 	std::make_heap(a, a + k);
@@ -172,9 +139,9 @@ void stl_makeheap(int a[], int n, const int k)
 	int maxe = a[0];
 	for (int i = k; i < n; ++i) {
 		if (a[i] < maxe) {
-			a[k] = a[i];
-			push_heap(a, a + k + 1);
-			pop_heap(a,  a + k + 1);
+			pop_heap(a, a + k);
+			a[k - 1] = a[i];
+			push_heap(a, a + k);
 			maxe = a[0];
 		}
 	}
@@ -186,7 +153,6 @@ void stl_makeheap(int a[], int n, const int k)
 void stl_priqueue(int a[], int n, const int k)
 {
 	reset(a, n);
-	rand_swap(a, n, k);
 	auto ts = getTime();
 
 	priority_queue<int> pri_queue;
@@ -197,8 +163,8 @@ void stl_priqueue(int a[], int n, const int k)
 	int maxe = pri_queue.top();
 	for (int i = k; i < n; i++) {
 		if (a[i] < maxe) {
-			pri_queue.push(a[i]);
 			pri_queue.pop();
+			pri_queue.push(a[i]);
 			maxe = pri_queue.top();
 		}
 	}
@@ -209,10 +175,9 @@ void stl_priqueue(int a[], int n, const int k)
 void max_heap(int a[], int n, const int k)
 {
 	reset(a, n);
-	rand_swap(a, n, k);
 	auto ts = getTime();
 
-	maxheap<int> my_heap;
+	maxheap<int> my_heap(k);
 	for (int i = 0; i < k; i++) {
 		my_heap.push(a[i]);
 	}
@@ -220,8 +185,8 @@ void max_heap(int a[], int n, const int k)
 	int maxe = my_heap.top();
 	for (int i = k; i < n; i++) {
 		if (a[i] < maxe) {
-			my_heap.push(a[i]);
 			my_heap.pop();
+			my_heap.push(a[i]);
 			maxe = my_heap.top();
 		}
 	}
@@ -230,13 +195,12 @@ void max_heap(int a[], int n, const int k)
 	printf("my max_heap     %4ld ms, a[%d] = %d, sum = %d\n", getTime() - ts, k, maxe, sum);
 }
 
-void radix_sort(int a[], int n, const int k)
+void bucket_sort(int a[], int n, const int k)
 {
 	reset(a, n);
-	rand_swap(a, n, k);
 	auto ts = getTime();
 
-	const unsigned int segment = 18;
+	const unsigned int segment = 20;
 	const unsigned int segsize = 1 << (32 - segment);
 	int bucket[segsize] = {0}; //32k
 	for (int i = 0; i < n; i++) {
@@ -258,21 +222,19 @@ void radix_sort(int a[], int n, const int k)
 			a[j++] = a[i];
 			if (j >= k * 2) {
 				std::sort(a, a + j);
-				j = k;
-				maxe = a[j - 1];
+				maxe = a[(j = k) - 1];
 			}
 		}
 	}
 
 	std::sort(a, a + j);
 	int sum = accumulate(a, a + k, 0);
-	printf("radix_sort       %4ld ms, a[%d] = %d, sum = %d\n", getTime() - ts, k, a[k - 1], sum);
+	printf("bucket_sort      %4ld ms, a[%d] = %d, sum = %d\n", getTime() - ts, j, a[k - 1], sum);
 }
 
 void merge_sort(int a[], int n, const int k)
 {
 	reset(a, n);
-	rand_swap(a, n, k);
 
 	auto ts = getTime();
 
@@ -284,9 +246,10 @@ void merge_sort(int a[], int n, const int k)
 	for (int i = k; i < n; i++) {
 		if (a[i] < maxe) {
 			best_a[bestn++] = a[i];
-			if (bestn == k / 16) {
+			if (bestn >= k / 16 + 10) {
 				std::sort(best_a, best_a + bestn);
 				std::inplace_merge(a, best_a, best_a + bestn);
+				//std::sort(a, best_a + bestn);
 				maxe = a[k - 1];
 				bestn = 0;
 			}
@@ -307,22 +270,23 @@ void merge_array(int a[], int b[], const int k)
 		return;
 	}
 
-	int i = k * 6 / 10, s = 64;
-	int n = k - 1 + s;
-	while (i > s && a[i - s] > b[n - i]) {
+	int i = k - 1, s = 128;
+	int j = k - 1 + s;
+	while (i > s && a[i - s] > b[j - i]) {
 		i -= s;
 	}
 
 	for (; i >= 0; i--) {
-		int j = k - 1 - i;
+		j = k - 1 - i;
+		//a[0, i]  b[0, j - 1]
 		if (a[i] <= b[j]) {
 #ifndef ME
-			int m = k - 1;
-			while (j > 0) {
-				if (a[i] > b[j])
-					a[m --] = a[i--];
+			int m = k - 1; j --;
+			while (j >= 0) {
+				if (a[i] <= b[j])
+					a[m --] = b[j--];
 				else
-					a[m --] = b[--j];
+					a[m --] = a[i--];
 			}
 #else
 			memcpy(a + i + 1, b, sizeof(int) * j);
@@ -336,7 +300,6 @@ void merge_array(int a[], int b[], const int k)
 void merge_sort2(int a[], int n, const int k)
 {
 	reset(a, n);
-	rand_swap(a, n, k);
 
 	auto ts = getTime();
 	std::sort(a , a + k);
@@ -361,26 +324,175 @@ void merge_sort2(int a[], int n, const int k)
 	printf("sort-merge2     %4ld ms, a[%d] = %d, sum = %d\n", getTime() - ts, k, maxe, sum);
 }
 
+static void cpuidInfo(int cpuinfo[4], int id)
+{
+#if _MSC_VER >= 1600 //2010
+	__cpuidex(cpuinfo, id, 0);
+#elif _MSC_VER >= 1400 //2005
+	__cpuid(cpuinfo, id);
+#elif __GNUC__ || __TINYC__
+	__asm__ ( "cpuid\n" :"=a"(cpuinfo[0]),"=b"(cpuinfo[1]),"=c"(cpuinfo[2]),"=d"(cpuinfo[3]) :"a"(id));
+#elif ASM_X86
+	__asm
+	{
+		mov eax, id
+		cpuid
+		mov edi, cpuinfo
+		mov dword ptr [edi + 0], eax
+		mov dword ptr [edi + 4], ebx
+		mov dword ptr [edi + 8], ecx
+		mov dword ptr [edi +12], edx
+	}
+#endif
+}
+
+static int getIntelL3info(int *l3size /*, uint *assoc, uint *linesize*/)
+{
+	int regs[4];
+	int i;
+
+	cpuidInfo(regs, 0); /* Maximum Input Value */
+	int max_leaf = regs[0];
+	if (max_leaf < 2) {
+		return -1; /* no way to find L3 cache info */
+	}
+
+	cpuidInfo(regs, 1); /* Additional Information */
+	int family = (regs[0] >> 8) & 0xF;
+	int model = (regs[0] >> 4) & 0xF;
+
+	cpuidInfo(regs, 2); /* Cache and TLB Information */
+
+	regs[0] &= 0xFFFFFF00; /* least significant byte of EAX is invalid */
+	for (i = 0; i < 4; i++) {
+		if (regs[i] < 0) { /* invalid if most significant bit set */
+			regs[i] = 0;
+		}
+	}
+
+	unsigned char *descriptors = (unsigned char *) regs;
+
+	const int mb = 1024;
+
+	#define RETINFO(s, a, l) *l3size = (s); /* *assoc = (a); *linesize = (l);*/ return 0
+
+	int use_leaf_4 = 0;
+	for (i = 0; i < 32; i++) {
+		switch(descriptors[i]) {
+			case 0x22: RETINFO(512 , 4, 64);
+			case 0x23: RETINFO(1 * mb, 8, 64);
+			case 0x25: RETINFO(2 * mb, 8, 64);
+			case 0x29: RETINFO(4 * mb, 8, 64);
+			case 0x40: RETINFO(0, 0, 0); /* no L3 cache */
+			case 0x46: RETINFO(4 * mb, 4, 64);
+			case 0x47: RETINFO(8 * mb, 8, 64);
+			case 0x49:
+					 if (family == 0x0F && model == 0x06) {
+						RETINFO(4 * mb, 16, 64);
+					 }
+					break;
+			case 0x4A: RETINFO(6 * mb, 12, 64);
+			case 0x4B: RETINFO(8 * mb, 16, 64);
+			case 0x4C: RETINFO(12 * mb, 12, 64);
+			case 0x4D: RETINFO(16 * mb, 16, 64);
+			case 0xD0: RETINFO(512 , 4, 64);
+			case 0xD1: RETINFO(1 * mb, 4, 64);
+			case 0xD2: RETINFO(2 * mb, 4, 64);
+			case 0xD6: RETINFO(1 * mb, 8, 64);
+			case 0xD7: RETINFO(2 * mb, 8, 64);
+			case 0xD8: RETINFO(4 * mb, 8, 64);
+			case 0xDC: RETINFO(1 * mb + 512, 12, 64);
+			case 0xDD: RETINFO(3 * mb, 12, 64);
+			case 0xDE: RETINFO(6 * mb, 12, 64);
+			case 0xE2: RETINFO(2 * mb, 16, 64);
+			case 0xE3: RETINFO(4 * mb, 16, 64);
+			case 0xE4: RETINFO(8 * mb, 16, 64);
+			case 0xEA: RETINFO(12 * mb, 24, 64);
+			case 0xEB: RETINFO(18 * mb, 24, 64);
+			case 0xEC: RETINFO(24 * mb, 24, 64);
+			case 0xFF:
+					use_leaf_4 = 1;
+					break;
+		}
+	}
+
+	if (!use_leaf_4 || max_leaf < 4) {
+		return -1; /* failed, no L3 info found */
+	}
+
+	i = 0;
+	while(1) {
+#if _MSC_VER >= 1400
+		__cpuidex(regs, 4, i); /* Deterministic Cache Parameters */
+#else
+		cpuidInfo(regs, 4); /* Deterministic Cache Parameters */
+#endif
+		if ((regs[0] & 0x1F) == 0) {
+			RETINFO(0, 0, 0); /* no L3 cache */
+		}
+		if (((regs[0] >> 5) & 0x7) == 3) {
+			int lsize = (regs[1] & 0xFFF) + 1;
+			int partitions = ((regs[1] >> 12) & 0x3FF) + 1;
+			int ways = ((regs[1] >> 22) & 0x3FF) + 1;
+			int sets = regs[2] + 1;
+			RETINFO(ways * partitions * lsize * sets, ways, lsize);
+		}
+		i++;
+	}
+
+	return 0;
+}
+
+static int getCpuInfo()
+{
+	char vendor[0x40] = {0};
+	int (*pTmp)[4] = (int(*)[4])vendor;
+	cpuidInfo(*pTmp ++, 0x80000002);
+	cpuidInfo(*pTmp ++, 0x80000003);
+	cpuidInfo(*pTmp ++, 0x80000004);
+
+	for (int i = 0; vendor[i]; i ++) {
+		if (vendor[i] != ' ' || vendor[i + 1] != ' ')
+			putchar(vendor[i]);
+	}
+
+	int cpuinfo1[4]; cpuidInfo(cpuinfo1, 0x80000005); //work for amd cpu
+	int cpuinfo2[4]; cpuidInfo(cpuinfo2, 0x80000006);
+
+	int l3Size = 0;
+	if (strstr(vendor, "Intel") != NULL && !strstr(vendor, "AMD")) {
+		getIntelL3info(&l3Size);
+	}
+	else {
+		l3Size = (cpuinfo2[3] >> 18) * 512;
+	}
+
+	printf(" Cpu Cache L1Dsize = %d kb, L2Size/L3Size = %d/%d kb\n", cpuinfo1[2] >> 24, cpuinfo2[2] >> 16, l3Size);
+	return 0;
+}
+
 static int arr[MAXN];
 int main(int argc, char* argv[])
 {
-	int maxn = MAXN, k = MAXK, type = 0;
+	int maxn = MAXN, k = 1000, type = 0;
 	srand(time(NULL));
 
-	printf("cmd:topk type(0(rand),1(decrease),2(increase),3(wavy)) k(<=%d) n(<=%d)\n\n", MAXK, MAXN);
+	getCpuInfo();
+	printf("cmd:topk k(<=%d) n(<=%d) type(0 rand,1 decrease,2 increase,3 wavy)\n\n", MAXK, MAXN);
 
-	if (argc > 1) { type = atoi(argv[1]); }
-	if (argc > 2) { k  = atoi(argv[2]);   if (k <= 0  || k > maxn || k > MAXK) k = MAXK;   }
-	if (argc > 3) { maxn = atoi(argv[3]); if (maxn <= 0 || maxn > MAXN) maxn = MAXN; }
+//	if (argc > 1) { type = atoi(argv[1]); }
+	if (argc > 1) { k  = atoi(argv[1]);   if (k <= 0  || k > maxn || k > MAXK) k = MAXK;   }
+	if (argc > 2) { maxn = atoi(argv[2]); if (maxn <= 0 || maxn > MAXN) maxn = MAXN; }
 	assert(k  <= MAXK && k < MAXN / 2);
 	assert(maxn <= MAXN);
 
-	int s = rand();
-	for (int j = 2; j > 0; j --) {
+	for (int j = 3; j >= 0; j --) {
+		int s = rand();
+		type = j;
 		for (int i = 0; i < maxn; i++) {
-			int r = rand() * rand() + rand() + s;
+			int r = rand() * rand() + s * rand() + i;
 			if (type == 1)
-				r = s + i;
+				r = -s + i;
 			else if (type == 2)
 				r = maxn - i - s;
 			else if (type == 3)
@@ -390,22 +502,15 @@ int main(int argc, char* argv[])
 			buff[i] = r;
 		}
 
-		printf("maxn = %d, topk = %d, type = %d\n\n", maxn, k, type);
-		for (int i = 0; i < maxn; i++) {
-			if (buff[i] < 0) {
-				buff[i] = 0 - buff[i];
-			}
-		}
-
-		stl_nth(arr, maxn, k);
-		radix_sort(arr, maxn, k);
-		stl_priqueue(arr, maxn, k);
+		printf("maxn = %d, topk = %d, type = %d\n", maxn, k, type);
+//		stl_nth(arr, maxn, k);
+//		bucket_sort(arr, maxn, k);
 		max_heap(arr, maxn, k);
+//		stl_priqueue(arr, maxn, k);
 		stl_makeheap(arr, maxn, k);
 		merge_sort2(arr, maxn, k);
-		merge_sort(arr, maxn, k);
-
-		putchar('\n');
+//		merge_sort(arr, maxn, k);
+		putchar('\n'); putchar('\n');
 	}
 
 	return 0;
